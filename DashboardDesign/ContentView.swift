8e18b34bc03705ce289c516417d1d7240f38b990
Shreeya Maskey <maskey.shriya@gmail.com>
2024-01-09 18:35:38 -0700

    changes

--- DashboardDesign/ContentView.swift
+++ DashboardDesign/ContentView.swift
@@ -8,14 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var currentZoom = 0.0
+    @State private var totalZoom = 1.0
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        Image("pf")
+            .scaleEffect(currentZoom + totalZoom)
+            .gesture(
+                MagnifyGesture()
+                    .onChanged { value in
+                        currentZoom = value.magnification - 1
+                    }
+                    .onEnded { value in
+                        totalZoom += currentZoom
+                        currentZoom = 0
+                    }
+            )
+            .accessibilityZoomAction { action in
+                if action.direction == .zoomIn {
+                    totalZoom += 1
+                } else {
+                    totalZoom -= 1
+                }
+            }
     }
 }
 
